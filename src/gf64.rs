33a8576fd716073115adc62f1712e254c07553a1
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-12-11 16:04:24 +0200

    Add GF64 tests

--- src/gf64.rs
+++ src/gf64.rs
@@ -1,7 +1,14 @@
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign};
+use std::fmt::Debug;
 
-#[derive(Clone, Copy)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub(crate) struct GF64(pub u64);
+
+impl Debug for GF64 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:#064b}", self.0)
+    }
+}
 
 // Prime polynomial for GF(2^64), x^64 term not included.
 // x^64 + x^4 + x^3 + x + 1
@@ -68,5 +75,59 @@ impl Div for GF64 {
 impl DivAssign for GF64 {
     fn div_assign(&mut self, rhs: Self) {
         *self = *self / rhs;
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::GF64;
+
+    pub fn gf64() -> GF64 {
+        GF64(fastrand::u64(0..))
+    }
+
+    pub fn gf64_array<const N: usize>() -> [GF64; N] {
+        std::array::from_fn(|_| gf64())
+    }
+
+    fn product(values: &[GF64]) -> GF64 {
+        let mut product = GF64(1);
+        for value in values {
+            product *= *value;
+        }
+        product
+    }
+
+    #[test]
+    fn addition_commutative() {
+        let a = gf64();
+        let b = gf64();
+        assert_eq!(a + b, b + a);
+    }
+
+    #[test]
+    fn multiplication_commutative() {
+        let a = gf64();
+        let b = gf64();
+        assert_eq!(a * b, b * a);
+    }
+
+    #[test]
+    fn invert() {
+        let a = gf64();
+        let b = gf64();
+        assert_eq!(a * a.invert(), GF64(1));
+        assert_eq!(a * b * b.invert(), a);
+        assert_eq!(a / b, a * b.invert());
+    }
+
+    #[test]
+    fn invert_many() {
+        let mut values = gf64_array::<1000>();
+        let prod = product(&values);
+        fastrand::shuffle(&mut values);
+        let inverses = values.map(GF64::invert);
+        let inv_prod = product(&inverses[1..]);
+        assert_eq!(prod * inv_prod, values[0]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
 mod gf64;
-use gf64::GF64;
-
-use fastrand::Rng;
 
 fn main() {
-    // Quick test of GF64
-    let mut rng = Rng::new();
-    let mut values: [GF64; 1000] = std::array::from_fn(|_| GF64(rng.u64(0..)));
-    let mut product = GF64(1);
-    for value in values {
-        product *= value;
-    }
-    // Galois field multiplication should be commutative.
-    rng.shuffle(&mut values);
-    for value in values {
-        product /= value;
-    }
-    assert_eq!(product.0, 1);
+    todo!();
 }
