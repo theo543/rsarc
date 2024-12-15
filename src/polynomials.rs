9873002997919506a2c185b090effc426841b512
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-12-15 18:51:21 +0200

    Add support for fixed x values (0, 1, 2, ...)

--- src/polynomials.rs
+++ src/polynomials.rs
@@ -25,8 +25,15 @@ fn update_divided_difference(div_diff: &mut [GF64], x_values: &[GF64], y: GF64, 
     }
 }
 
-pub fn newton_interpolation(y_values: &[GF64], x_values: &[GF64], poly: &mut [GF64], memory: &mut [GF64]) {
-    assert_eq!(y_values.len(), x_values.len());
+fn update_divided_difference_fixed_x_values(div_diff: &mut [GF64], y: GF64, i: usize) {
+    div_diff[i] = y;
+    for j in (0..i).rev() {
+        div_diff[j] = (div_diff[j+1] + div_diff[j]) / (GF64(i as u64) + GF64(j as u64));
+    }
+}
+
+pub fn newton_interpolation(y_values: &[GF64], x_values: Option<&[GF64]>, poly: &mut [GF64], memory: &mut [GF64]) {
+    if let Some(x_values) = x_values { assert_eq!(y_values.len(), x_values.len()); }
     assert_eq!(y_values.len(), poly.len());
     assert!(memory.len() >= y_values.len() * 2);
     let (basis_poly, remaining_memory) = memory.split_at_mut(y_values.len());
@@ -38,8 +45,13 @@ pub fn newton_interpolation(y_values: &[GF64], x_values: &[GF64], poly: &mut [GF
     poly[0] = y_values[0];
     poly[1..].fill(GF64(0));
     for i in 1..y_values.len() {
-        multiply_by_x_plus_a(&mut basis_poly[..=i], x_values[i - 1]);
-        update_divided_difference(div_diff, x_values, y_values[i], i);
+        if let Some(x_values) = x_values {
+            multiply_by_x_plus_a(&mut basis_poly[..=i], x_values[i - 1]);
+            update_divided_difference(div_diff, x_values, y_values[i], i);
+        } else {
+            multiply_by_x_plus_a(&mut basis_poly[..=i], GF64((i - 1) as u64));
+            update_divided_difference_fixed_x_values(div_diff, y_values[i], i);
+        }
         add_poly_with_mul(&mut poly[..=i], &basis_poly[..=i], div_diff[0]);
     }
 }
@@ -97,8 +109,19 @@ mod tests{
         let y_vals: [GF64; 50] = gf64_array();
         let x_vals: [GF64; 50] = distinct_gf64_array();
         let mut poly = gf64_array::<50>();
-        newton_interpolation(&y_vals, &x_vals, &mut poly, &mut memory);
+        newton_interpolation(&y_vals, Some(&x_vals), &mut poly, &mut memory);
         let roundtrip = x_vals.map(|x| evaluate_poly(&poly, x));
+        assert_eq!(y_vals, roundtrip);
+    }
+
+    #[test]
+    fn can_roundtrip_fixed_x_values() {
+        let mut memory = gf64_array::<100>();
+        let y_vals: [GF64; 50] = gf64_array();
+        let mut poly = gf64_array::<50>();
+        newton_interpolation(&y_vals, None, &mut poly, &mut memory);
+        let x_values: [GF64; 50] = std::array::from_fn(|x| GF64(x as u64));
+        let roundtrip = x_values.map(|x| evaluate_poly(&poly, x));
         assert_eq!(y_vals, roundtrip);
     }
 
@@ -111,7 +134,7 @@ mod tests{
         let original_x_values: [GF64; 50] = (&all_x_values[..50]).try_into().unwrap();
 
         let mut original_poly = [GF64(0); 50];
-        newton_interpolation(&original_data, &original_x_values, &mut original_poly, &mut memory);
+        newton_interpolation(&original_data, Some(&original_x_values), &mut original_poly, &mut memory);
 
         let mut all_y_values = [GF64(0); 100];
         let (data, parity) = all_y_values.split_at_mut(50);
@@ -121,7 +144,7 @@ mod tests{
         shuffle_together(&mut all_x_values, &mut all_y_values);
 
         let mut recovered_poly = [GF64(0); 50];
-        newton_interpolation(&all_y_values[..50], &all_x_values[..50], &mut recovered_poly, &mut memory);
+        newton_interpolation(&all_y_values[..50], Some(&all_x_values[..50]), &mut recovered_poly, &mut memory);
         let recovered_data = original_x_values.map(|x| evaluate_poly(&recovered_poly, x));
 
         assert_eq!(original_poly, recovered_poly);
@@ -131,6 +154,6 @@ mod tests{
     #[test]
     #[should_panic]
     fn insufficient_memory() {
-        newton_interpolation(&[GF64(0); 10], &[GF64(0); 10], &mut [GF64(0); 10], &mut [GF64(0); 19]);
+        newton_interpolation(&[GF64(0); 10], None, &mut [GF64(0); 10], &mut [GF64(0); 19]);
     }
 }
