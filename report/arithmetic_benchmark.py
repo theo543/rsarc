8f32d2e636c8bf7f813c0833d535c8484ba630f3
Theodor Negrescu <theodor.negrescu@gmail.com>
2025-06-16 01:26:32 +0300

    Run 2^28 arithmetic benchmark, remove LZCNT column which shows nothing interesting

--- report/arithmetic_benchmark.py
+++ report/arithmetic_benchmark.py
@@ -2,18 +2,16 @@ from os import system
 from pathlib import Path
 import csv
 
-POW = 26
+POW = 28
 SYMBOL_NUM = 2 ** POW
 
 
-FILES = [Path(x).with_suffix(f".{POW}.csv") for x in ["math_benchmarks_default", "math_benchmarks_no_clmul", "math_benchmarks_no_clmul_no_lzcnt"]]
+FILES = [Path(x).with_suffix(f".{POW}.csv") for x in ["math_benchmarks_default", "math_benchmarks_no_clmul"]]
 
 if not FILES[0].exists():
     system(f"cargo run --release -F math_benchmarks math_benchmarks {SYMBOL_NUM} > {FILES[0]}")
 if not FILES[1].exists():
     system(f"cargo run --release -F math_benchmarks -F no_clmul_check math_benchmarks {SYMBOL_NUM} > {FILES[1]}")
-if not FILES[2].exists():
-    system(f"cargo run --release -F math_benchmarks -F no_clmul_check -F no_lzcnt_check math_benchmarks {SYMBOL_NUM} > {FILES[2]}")
 
 results = [[float("nan")] * 3 for _ in range(3)]
 for i, file in enumerate(FILES):
@@ -24,16 +22,16 @@ for i, file in enumerate(FILES):
             assert row[0] == name
             results[i][j] = round(float(row[2]), ndigits=2)
 
-table_results = f"""Multiplication     & {results[0][0]} ns & {results[1][0]} ns & {results[2][0]} ns \\\\
-Inversion (Euclid) & {results[0][1]} ns & {results[1][1]} ns & {results[2][1]} ns \\\\
-Inversion (Pow)    & {results[0][2]} ns & {results[1][2]} ns & {results[2][2]} ns \\\\
+table_results = f"""Multiplication     & {results[0][0]} ns & {results[1][0]} ns \\\\
+Inversion (Euclid) & {results[0][1]} ns & {results[1][1]} ns \\\\
+Inversion (Pow)    & {results[0][2]} ns & {results[1][2]} ns \\\\
 """
 
 table = """\\begin{table}[hbt]
 \\centering
-\\begin{tabular}{lrrr}
+\\begin{tabular}{lrr}
 \\toprule
-\\textbf{Operation} & \\textbf{Default} & \\textbf{No CLMUL} & \\textbf{No CLMUL \\& LZCNT} \\\\
+\\textbf{Operation} & \\textbf{Default} & \\textbf{No CLMUL} \\\\
 \\midrule
 """ \
 + table_results + \
