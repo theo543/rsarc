92c3f202d54eb4b8bfdd24bb650a55f82ad97f28
Theodor Negrescu <theodor.negrescu@gmail.com>
2025-06-15 11:24:24 +0300

    Add decoder benchmark and CPU time measurement

--- report/full_benchmark.py
+++ report/full_benchmark.py
@@ -1,7 +1,10 @@
 from os import system, urandom
 from time import sleep, time_ns
-import ctypes
 from pathlib import Path
+from sys import argv
+from random import randint
+import threading
+import psutil
 
 def clear_file_cache():
     # Et = Empty Standby List
@@ -16,16 +19,55 @@ def gen_random(f: Path, size: int):
         if size % CHUNK != 0:
             file.write(urandom(size % CHUNK))
 
-def kb(x: int): return x * 1024, f"KB{x}.bin"
-def mb(x: int): return kb(x)[0] * 1024, f"MB{x}.bin"
-def gb(x: int): return mb(x)[0] * 1024, f"GB{x}.bin"
+BLOCK = 16384
+PARITY_PERCENT = 20
+
+def corrupt_file(f: Path, p: Path):
+    f_size = f.stat().st_size
+    p_size = p.stat().st_size
+    blocks = (f_size + BLOCK - 1) // BLOCK
+    parity_blocks = ((blocks + PARITY_PERCENT - 1) * PARITY_PERCENT) // 100
+    max_errors = (blocks * PARITY_PERCENT) // 100
+    with open(f, "rb+") as file:
+        for _ in range(max_errors // 2):
+            block_idx = randint(0, blocks)
+            file.seek(block_idx * BLOCK)
+            remaining_size = f_size - block_idx * BLOCK
+            file.write(urandom(BLOCK)[:remaining_size])
+    with open(p, "rb+") as file:
+        metadata_size = 100 + (blocks + parity_blocks) * 40
+        for _ in range(max_errors // 2):
+            block_idx = randint(0, blocks)
+            file.seek(metadata_size + block_idx * BLOCK)
+            remaining_size = p_size - metadata_size - block_idx * BLOCK
+            file.write(urandom(BLOCK)[:remaining_size])
+
+def cpu_measure_thread(stop_event: threading.Event, out: list[int]):
+    SAMPLING_RATE = 0.2 # seconds
+    SAMPLING_RATE_NS = 2 * 10**8 # 0.2 * 10^9
+    CPU_THRESHOLD = 15
+    high_usage_time = 0
+    while not stop_event.is_set():
+        cpu_usage = psutil.cpu_percent(interval=SAMPLING_RATE)
+        if cpu_usage > CPU_THRESHOLD:
+            high_usage_time += SAMPLING_RATE_NS
+    out.append(high_usage_time)
+
+def run_timed(command: str, name: str, log_file: Path):
+    stop_event = threading.Event()
+    out = []
+    cpu_thread = threading.Thread(target=cpu_measure_thread, args=(stop_event, out), daemon=True)
+    start = time_ns()
+    cpu_thread.start()
+    assert system(command) == 0, f"Command failed: {command}"
+    stop_event.set()
+    cpu_thread.join()
+    duration = time_ns() - start
+    with open(log_file, "a", encoding="ascii") as log:
+        log.write(f"{name},{duration},{out[0]}\n")
 
 def main():
-    assert ctypes.windll.shell32.IsUserAnAdmin() != 0, "This script must be run as an admin since flushing the file cache using RAMMap64 requires admin privileges."
-
-    files = [gb(2 ** x) for x in range(6, 8)]
-    BLOCK = 16384
-    PARITY_PERCENT = 20
+    files = [(1024 * 1024 * 1024 * (2 ** x), f"GB{2 ** x}.bin") for x in range(0, 7)]
     parity_count = [(size // BLOCK) * PARITY_PERCENT // 100 for size, _ in files]
 
     for (s, f) in files:
@@ -34,14 +76,20 @@ def main():
             print(f"Generating {f}")
             gen_random(f, s)
 
-    log = Path("benchmark.log")
+    log = Path("end_to_end_benchmark.csv")
 
-    for ((s, f), p) in zip(files, parity_count):
-        clear_file_cache()
-        start = time_ns()
-        assert system(f"cargo run --release encode {f} {f}.rsarc {BLOCK} {p}") == 0
-        with open(log, "a", encoding="ascii") as log_file:
-            log_file.write(f"encode {f}: {time_ns() - start} ns\n")
+    if "encode" in argv:
+        for ((s, f), p) in zip(files, parity_count):
+            clear_file_cache()
+            run_timed(f"cargo run --release encode {f} {f}.rsarc {BLOCK} {p}", f"encode,{s // (1024 * 1024 * 1024)}", log)
+
+    if "decode" in argv:
+        for ((s, f), p) in zip(files, parity_count):
+            data = Path(f)
+            parity = Path(f"{f}.rsarc")
+            corrupt_file(data, parity)
+            clear_file_cache()
+            run_timed(f"cargo run --release repair {data} {parity}", f"decode,{s // (1024 * 1024 * 1024)}", log)
 
 if __name__ == "__main__":
     main()
